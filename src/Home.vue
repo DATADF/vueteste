c49b2201ce7cc0af561a898943221fb5447179a5
DATADF <klaytonfantin@outlook.com>
2019-07-08 10:44:25 -0300

    App Services

---
+++ src/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="home">
+        <Services mensagem="Meu primeiro componente Vue.js"/>
+    </div>
+</template>
+
+
+<script>
+import Services  from "./components/Services";
+
+export default {
+    name: 'home',
+    components: {
+        Services
+    }
+}
+</script>
---
+++ src/components/Services.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h2>{{ mensagem }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Services',
+    props: {
+    mensagem: String
+  }
+}
+</script>
+
+<style scoped>
+    .container {
+        width: 100%;
+        max-width: 960px;
+        margin: auto;
+    }
+
+    h2 {
+        color: orangered;
+    }
+</style>
+
